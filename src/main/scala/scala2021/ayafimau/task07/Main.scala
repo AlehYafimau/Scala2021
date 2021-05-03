249787f5244ec73fc2b81d3e3820714987fa586e
Aleh Yafimau <aleh.yafimau@gmail.com>
2021-05-03 15:14:37 +0300

    call the release on an object only if it got initialized successfully (task 7)

--- src/main/scala/scala2021/ayafimau/task07/Main.scala
+++ src/main/scala/scala2021/ayafimau/task07/Main.scala
@@ -4,39 +4,59 @@ import java.io.{File, FileInputStream}
 
 object Main extends App {
 
-  withResource(Connection(port = 9000)) {
+  withResource {
+    Connection(port = 9000)
+  } {
     conn => conn.run()
-  }(conn => if (conn != null) conn.close())
+  } {
+    _.close()
+  }
 
   val tempFile = File.createTempFile("pre-", ".txt")
   tempFile.deleteOnExit()
   val filePath = tempFile.toPath.toString
 
   withResource(new File(filePath)) {
-    file => file.exists()
-  }_
+    file => {
+      file.exists()
+    }
+  }()
 
-  withResource(new FileInputStream(filePath)){
+  withResource(new FileInputStream(filePath)) {
     fis => {
       val bytes = fis.available()
       println(s"Count bytes are $bytes")
     }
-  }(fis => if (fis != null) fis.close())
+  }(_.close())
 
   //automatic resource management
   def withResource[T, U]
-  (resource: T)
+  (obtainResource: => T)
   (block: T => U)
-  (release: T => Unit = (_:T) => ()) = {
-    try {
-      block(resource)
-    } finally {
-      release(resource)
+  (release: T => Unit = (_: T) => ()) = {
+
+    val tryObtainResource = try {
+      Right(obtainResource)
+    } catch {
+      case e: Exception => Left(e)
+    }
+
+    tryObtainResource match {
+      case Right(resource) =>
+        try {
+          block(resource)
+        } finally {
+          release(resource)
+        }
+      //nothing to release if we failed to obtain the resource itself
+      case Left(e) => throw e
     }
   }
 
   case class Connection(port: Int) {
     def close(): Unit = println("Closed")
+
     def run(): Unit = println("Run")
   }
+
 }
